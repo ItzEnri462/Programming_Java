c7fdf4dfaf24bba76cf7b8dc3a3f0c920c63fe21
ItzEnri462 <enrico.lucia2009@icloud.com>
2026-09-19 09:26:41 +0200

    primo utilizzo della classe lampadina

--- progetto_lampadina/src/Main.java
+++ progetto_lampadina/src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Lampadina l;
+        Lampadina l, a;
+
         l = new Lampadina();
+        a = new Lampadina();
+
         l.potenza = 30;
+        a.potenza = 53;
     }
 }
