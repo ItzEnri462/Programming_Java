3d43f7a61263fd4abd95a2b724e44bfb3cde6a50
ItzEnri462 <enrico.lucia2009@icloud.com>
2026-09-22 12:47:06 +0200

    prima prova del metodo costruttore con l'utilizzo della classe Studente

--- prove/studenti/src/Main.java
+++ prove/studenti/src/Main.java
@@ -1,10 +1,8 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-    Studente s = new Studente();
-
-    s.nome = "Davide";
-    s.cognome = "Rossi";
-    s.altezza = (float) 1.89;
-    s.eta = 18;
+        Studente s = new Studente("Davide", "Rossi", 17, (float) 1.72);
+        System.out.println(s.nome);
     }
 }
--- prove/studenti/src/Studente.java
+++ prove/studenti/src/Studente.java
@@ -3,4 +3,11 @@ public class Studente {
     String cognome;
     float altezza;
     int eta;
+
+    public Studente(String name, String surname, int age, float height)  {
+        nome=name;
+        cognome=surname;
+        eta=age;
+        altezza=height;
+    }
 }
