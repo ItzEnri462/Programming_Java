039f1c9a95f9457cfb76fd4c98af32ed70e611a0
ItzEnri462 <enrico.lucia2009@icloud.com>
2026-09-23 17:35:10 +0200

    creazione della classe lampadinaIntelligente con creazione del metodo costruttore

---
+++ compiti/progetto_lampadina_v2/src/lampadinaIntelligente.java
@@ -0,0 +1,16 @@
+public class lampadinaIntelligente {
+    private int potenza;
+    private int intensita;
+    private String colore;
+    private String nome;
+    private boolean accesa;
+
+    public lampadinaIntelligente(int potenza) {
+        this.potenza = potenza;
+        this.nome = "";
+        this.intensita = 50;
+        this.colore = "bianco";
+        this.accesa = false;
+    }
+
+}
