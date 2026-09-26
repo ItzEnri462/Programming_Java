cc58f2536ea0e7d1cd0ed58714b12f98f1f1fd82
ItzEnri462 <enrico.lucia2009@icloud.com>
2026-09-26 09:14:54 +0200

    creazione del menu a console per inserire i dati dei due studenti

--- prove/studenti/src/Main.java
+++ prove/studenti/src/Main.java
@@ -2,13 +2,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Studente s = new Studente("Davide", "Rossi", 17, (float) 1.72, (float) 53.9);
-        int nStudente;
+        Studente a = new Studente();
+        Studente b = new Studente();
+        System.out.println("Inserire i dati del primo studente in ordine: \n  1. nome \n  2. cognome \n  3. altezza \n  4. eta \n 5.  peso");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Inserire il numero di studenti");
-        nStudente = sc.nextInt();
-        for (int i = 0; i < nStudente; i++) {
+        a.nome = sc.next();
+        a.cognome = sc.next();
+        a.altezza = sc.nextFloat();
+        a.eta = sc.nextInt();
+        a.peso = sc.nextFloat();
 
-        }
+        System.out.println("Inserire i dati del secondo studente in ordine: \n  1. nome \n  2. cognome \n  3. altezza \n  4. eta \n 5.  peso");
+        b.nome = sc.next();
+        b.cognome = sc.next();
+        b.altezza = sc.nextFloat();
+        b.eta = sc.nextInt();
+        b.peso = sc.nextFloat();
+
     }
 }
