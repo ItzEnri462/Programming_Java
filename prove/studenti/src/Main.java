5789be789fb628af19f947901411e769ce3b74b5
ItzEnri462 <enrico.lucia2009@icloud.com>
2026-09-26 08:34:50 +0200

    creazione del metodo per il calcolo del bmi

--- prove/studenti/src/Main.java
+++ prove/studenti/src/Main.java
@@ -2,7 +2,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        Studente s = new Studente("Davide", "Rossi", 17, (float) 1.72);
+        Studente s = new Studente("Davide", "Rossi", 17, (float) 1.72, (float) 53.9);
         System.out.println(s.nome);
     }
 }
--- prove/studenti/src/Studente.java
+++ prove/studenti/src/Studente.java
@@ -3,15 +3,26 @@ public class Studente {
     String cognome;
     float altezza;
     int eta;
-    public Studente(String name, String surname, int age, float height)  {
-        nome=name;
-        cognome=surname;
-        eta=age;
-        altezza=height;
+    float peso;
+
+    public Studente(String nome, String cognome, int eta, float altezza, float peso) {
+        this.nome = nome;
+        this.cognome = cognome;
+        if (this.eta <= 5)
+            eta = 5;
+        this.eta = eta;
+        this.altezza = altezza;
+        this.peso = peso;
     }
-    public String toString (){
+    public String toString () {
         String s = "I dati dello studente sono: ";
         s += this.nome + ", " + this.cognome + ", " + this.eta + ", " + this.altezza + ".";
         return s;
+    }
+
+    public double calcolaIndice () {
+        //bmi = peso / h * 2
+        double bmi = peso / (altezza * 2);
+        return bmi;
     }
 }
